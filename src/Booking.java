010123ec9cf9544d66d3c146cc115581fd6a7d79
Jeff Raghav <windowpane1712@gmail.com>
2017-03-08 14:08:46 +0800

    First commit
    All the files added

---
+++ src/Booking.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jeff
+ */
+import java.util.Date;
+import java.text.SimpleDateFormat;
+public class Booking {
+    private String name;
+    private Date date;
+    
+    public Booking(String name, Date date){
+        this.name = name;
+        this.date = date;
+    }
+    
+    public Date getDate(){
+        return date;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    
+    //returns true if bookings are same, false if bookings are not same
+    
+    public boolean equals(Booking booking){
+        if(booking.getDate().equals(this.date)&& booking.getName().equals(this.name)){
+            return true;
+        }
+        
+        else{
+            return false;
+        }
+    }
+   
+}
---
+++ src/DateTest.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jeff
+ */
+import java.text.ParseException;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class DateTest {
+
+    public static void main(String[] args){
+        
+        SimpleDateFormat ft = new SimpleDateFormat("MM/dd/yy");
+        try {
+            Date date = ft.parse("01/29/02");
+            Date now = new Date();
+            System.out.println(date.toString());
+            System.out.println("Now: " + now.toString());
+            System.out.println(date.compareTo(now));
+        } catch (ParseException ex) {
+            Logger.getLogger(DateTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+    }
+}
