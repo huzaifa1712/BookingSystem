c96e6ebf874bd41c3a2928351827ac5da88b31f9
Jeff Raghav <windowpane1712@gmail.com>
2017-03-21 09:25:02 +0800

    Calendar, Booking method

--- src/Booking.java
+++ src/Booking.java
@@ -10,26 +10,34 @@
  */
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 public class Booking {
     private String name;
-    private Date date;
+    private Calendar date;
     private SimpleDateFormat format = new SimpleDateFormat("dd/mm/yy");
     
-    public Booking(String name, Date date){
+    public Booking(String name, Calendar date){
         this.name = name;
         this.date = date;
     }
     
-    public Date getDate(){
+    public Calendar getDateObject(){
         return date;
+    }
+    
+    public void printDateFormatted(){
+        System.out.println(format.format(date.getTime()));
     }
     
     public String getName(){
         return name;
     }
     
-    //method to convert current selections into Date object
-    public Date toDate()
+    //method to convert current selections into Calendar object
+    public Calendar toCalendar(){
+        
+    }
     
     //returns true if bookings are same, false if bookings are not same
     
